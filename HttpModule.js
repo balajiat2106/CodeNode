9ee491ceef0df9b169a3f6e602184430986b19f8
balajiat2106 <balaji.prabakaran87@gmail.com>
2019-11-18 00:58:51 +0800

    'HttpModule'

---
+++ HttpModule.js
@@ -0,0 +1,22 @@
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+
+if(req.url==='/'){
+    res.write('Hello Http');
+res.end();
+}
+if(req.url==='/api/'){
+    res.write('Hello Http New')
+    res.end();
+}
+});
+
+server.on('connection', (socket)=>{
+    console.log('New connection...');
+});
+
+server.listen(2000);
+
+console.log('Listening to port 2000');
+
