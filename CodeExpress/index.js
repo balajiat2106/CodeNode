a391850b881a28b4636b0c3f74d8c7cbd74fd79c
balajiat2106 <balaji.prabakaran87@gmail.com>
2019-12-10 05:43:56 +0800

    basic express get routing

--- CodeExpress/index.js
+++ CodeExpress/index.js
@@ -1,12 +1,25 @@
 const express=require('express');
 const app=express();
 
+let courses=[{id:1,name:"course 1"},{id:2,name:"course 2"},{id:3,name:"course 3"}];
+
 app.get('/',(req,res)=>{
-    res.send("Base url");
+res.send('Base page reloaded');
 });
 
-app.get('/api',(req,res)=>{
-    res.send("API url");
+app.get('/next',(req,res)=>{
+    res.send(JSON.stringify([1,2,3]));
+})
+
+app.get('/next/:id',(req,res)=>{
+    let course=courses.find(c=>c.id===parseInt(req.params.id));
+    if(!course)res.status(404).send('Not found!')    
+    res.send(course);
 });
 
-app.listen(3000,()=>console.log("Listening on port 3000..."));
+app.get('/next/:id',(req,res)=>{
+    res.send(req.params);
+});
+
+var port=process.env.port||200;
+app.listen(port,()=>{console.log(`Listening on ${port}`)});
